88b7346c0f326f7f43b9b39fbfe82126b8678335
maciek11123 <maciekwilkos@gmail.com>
2026-06-16 16:13:11 -0400

    Update notes/sw.js

---
+++ notes/sw.js
@@ -0,0 +1,10 @@
+const CACHE = 'notes-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./'])).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
